978238258d996852534f4248ae6b0991dff86064
Tyson Jouglet <tyson.jouglet@gmail.com>
2020-12-27 22:42:33 -0600

    initial version

--- index.js
+++ index.js
@@ -1,25 +1,68 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const path = require('path');
 const TOKEN = process.env.TOKEN;
+
+const paths ={
+  audio : null
+}
+
+const prefix = '!';
 
 bot.login(TOKEN);
 
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
+
+  paths.audio = path.join(__dirname, 'audio');
 });
 
-bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
+async function getConnection(message){
+  if (message.member.voice.channel) {
+    return await message.member.voice.channel.join();
+  }
+  return undefined;
+}
 
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
-    }
+bot.on('message', async message => {
+
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  console.info('message received');
+  if (!message.content.startsWith(prefix)) return;
+  if (!message.guild) return;
+
+  connection = await getConnection(message);
+
+  if(!connection) return;
+
+  if (message.content === '!doit') {
+    const dispatcher = connection.play(path.join(paths.audio,'doit.mp3'), {
+      volume: 1.5,
+    });
+  }else if(message.content === '!will'){
+    const dispatcher = connection.play(path.join(paths.audio,'will.mp3'), {
+      volume: 1.2,
+    });
+  }else if(message.content === '!nice'){
+    const dispatcher = connection.play(path.join(paths.audio,'nice.mp3'), {
+      volume: 1.2,
+    });
+  }else if(message.content === '!balls'){
+    const dispatcher = connection.play(path.join(paths.audio,'balls.mp3'), {
+      volume: 1.2,
+    });
+  }else if(message.content === '!plums'){
+    const dispatcher = connection.play(path.join(paths.audio,'plums.mp3'), {
+      volume: 1.2,
+    });
+  }else if(message.content === '!watch'){
+    const dispatcher = connection.play(path.join(paths.audio,'watch.mp3'), {
+      volume: 1.2,
+    });
+  }
+  else if(message.content === '!begone'){
+    message.member.voice.channel.leave();
   }
 });
